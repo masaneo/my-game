6f878a9bd1368428e57d747f92020976091e4d78
Kamil Bąba <kamilbaba258@gmail.com>
2022-12-28 15:20:01 +0100

    add coins, itemcollector, collecting coins, ui text showing collected coins, flag coin condition to go to next level

--- Assets/Scripts/FlagScript.cs
+++ Assets/Scripts/FlagScript.cs
@@ -2,12 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class FlagScript : MonoBehaviour
 {
+    public ItemCollector itemCollector;
+    public int coinsOnLevel;
+    [SerializeField] private TextMeshProUGUI missingCoinsText;
+
+    IEnumerator ShowMissingCoinsMessage(int seconds){
+        missingCoinsText.enabled = true;
+        yield return new WaitForSeconds(seconds);
+        missingCoinsText.enabled = false;
+    }
+
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player") {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            if(itemCollector.coinCount == coinsOnLevel) {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                itemCollector.coinCount = 0;
+            }
+            else {
+                StartCoroutine(ShowMissingCoinsMessage(3));
+            }
         }
     }
 }
---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    public int coinCount = 0;
+    [SerializeField] private TextMeshProUGUI coinCountText; 
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if(collision.gameObject.CompareTag("Coin")) {
+            Destroy(collision.gameObject);
+            coinCount++;
+            coinCountText.text = "Collected coins: " + coinCount;
+        }
+    }
+
+}
